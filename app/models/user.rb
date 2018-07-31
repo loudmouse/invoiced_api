618c28c560f4f97b9dd751d6e4b1b31fefb57b72
Nolan Neeley <nolanneeley@gmail.com>
2018-07-31 18:01:05 -0500

    scoped the controller actions so the logged in user only sees his resources

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
- class User < ApplicationRecord
+class User < ApplicationRecord
   acts_as_token_authenticatable
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :contacts
+
+
 end
