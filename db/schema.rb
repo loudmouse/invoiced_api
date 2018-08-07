93fda6bb8176d572f5273f12d6e49e26dc2cdad2
Nolan Neeley <nolanneeley@gmail.com>
2018-08-07 09:05:13 -0500

    added reconfirmable column to users table and updated user create view to include authentication, confirmation token, and confirmed at time fields for consumption by front end

--- db/schema.rb
+++ db/schema.rb
@@ -64,6 +64,7 @@ ActiveRecord::Schema.define(version: 2018_08_07_132639) do
     t.string "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
     t.index ["authentication_token"], name: "index_users_on_authentication_token", unique: true
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
