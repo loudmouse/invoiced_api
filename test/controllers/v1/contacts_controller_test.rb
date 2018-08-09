c5e7b1b9ae64ef7ac480a9d97113babbdea14246
Nolan Neeley <nolanneeley@gmail.com>
2018-08-09 07:24:59 -0500

    setup fixtures for accounts, contacts, and users with faker gem. then wrote functional tests for the accounts and contacts controllers

--- test/controllers/v1/contacts_controller_test.rb
+++ test/controllers/v1/contacts_controller_test.rb
@@ -1,7 +1,22 @@
+# frozen_string_literal: true
+
 require 'test_helper'
 
-class V1::ContactsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+module V1
+  class ContactsControllerTest < ActionDispatch::IntegrationTest
+    test 'should get only contacts for loudmouse' do
+      account = accounts(:loudmouse)
+
+      not_loudmouse_contact = contacts(:two)
+
+      get v1_contacts_path(account_id: account.id)
+
+      contacts = JSON.parse(@response.body)['data']
+      contact_ids = contacts.map { |contact| contact['id']}
+
+      assert_response :success
+      assert_not_includes contact_ids, not_loudmouse_contact.id
+
+    end
+  end
 end
